25e3c982dc1aee7b65bbcf4b94aa8a17239de62b
SlimeLimeDeveloler <slimelime00123@gmail.com>
2024-07-23 20:27:36 +0700

    Загрузка проект на GitHub

---
+++ app/src/main/java/test/openg/MainActivity.java
@@ -0,0 +1,23 @@
+package test.openg;
+
+import android.app.Activity;
+import android.os.Bundle;
+import test.openg.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+	
+	  private ActivityMainBinding binding;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		    binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.binding = null;
+    }
+}
